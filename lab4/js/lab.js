3db1262b063bbf76073f5aabadd0b0b600cf009e
sbathaee <sbathaee@ucsc.edu>
2022-04-16 14:06:33 -0700

    lab4 complete

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+// Summary: Lab 4 pseudocode - Kenny Losier and Shayan Bathaee
+// Description: This file contains pseudocode for our chosen ncomputer task (parsing
+//              a file and replacing the delimiter with a new line)
+// @link   https://canvas.ucsc.edu/courses/52418/assignments/330575
+// @file   lab.js - The file you are in right now. Contains the pseudocode
+//         index.html - Contains description of the lab, including challenges, problems
+//                      and results. This file also contains pseudocode from task 1 & task 2.
+//         lab.css - This file contains the stylesheet for index.html
+// @author Kenny Losier & Shayan Bathaee
+// @since  4.16.2022
+
+// Pseudocode: Parse File and Replace Delimiter With New Line:
+// Retrieve delimiter from user
+// Retrieve file name from user
+// Open file for reading
+// For character in file:
+    // If character = delimiter
+    // Print newline
+    // Else: print character
+// View results

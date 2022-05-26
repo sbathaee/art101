aa2ea370836f44589b58db123550490aa2bffccd
sbathaee <sbathaee@ucsc.edu>
2022-05-25 17:46:33 -0700

    lab 15

---
+++ lab15/js/lab.js
@@ -0,0 +1,41 @@
+/*
+ * Author: Shayan Bathaee & Kenny Losier
+ * Created: 25 May, 2022
+ * Description: This file contains the JavaScript code for our Lab 15 Assignment
+ * Files:
+ *  lab.js - The file you are in right now. Contains the JavaScript code for this lab
+ *  index.html - Contains description of the lab, including challenges, problems
+ *               and results. This file also contains the linked JavaScript file
+ *           and CSS file.
+ *  lab.css - This file contains the stylesheet for index.html
+ *
+ * Licence: Public Domain
+ */
+
+$("#activate").click(function() {
+  var randomID = Math.floor(Math.random() * 2624); // generate a random comic ID up to their most recent comic
+  var img_link;
+  $.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://xkcd.com/" + randomID + "/info.0.json",
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    success: function(data) {
+      // insert the comic image into the output div
+      if ($("#comicImg").length == 0) {
+        $("#output").append("<img src='" + img_link + "' alt='comic' width=600px id='comicImg'>");
+      }
+      else {
+        $("#comicImg").replaceWith("<img src='" + img_link + "' alt='comic' width=600px id='comicImg'>");
+      }
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+      // log the error
+      console.log("Error:", textStatus, errorThrown);
+    }
+  })
+});
